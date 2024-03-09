7e1d90faa73cbc8da9cf9ffbdd3dc641ffa105c1
Focus-Alfasarana <161445528+Focus-Alfasarana@users.noreply.github.com>
2024-03-09 07:52:31 +0000

    modified:   src/Homepage.jsx

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -290,6 +290,125 @@ function CompanyWork() {
     );
 }
 
+function CompanyAddrs() {
+    return (
+        <li class="flex flex-col items-center justify-center">
+            <div class="flex h-10 w-10 items-center justify-center rounded bg-pink-900 dark:bg-pink-600 text-gray-50">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="h-6 w-6"
+                >
+                    <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
+                    <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z"></path>
+                </svg>
+            </div>
+            <div>
+                <h3 class="py-3 mb-2 text-lg font-medium leading-6 text-gray-900 dark:text-white">
+                    Alamat Kami
+                </h3>
+                <p class="text-gray-600 text-base dark:text-slate-400">
+                    Medokan Asri Timur RL V/i-16
+                </p>
+                <p class="text-gray-600 text-base dark:text-slate-400">
+                    Rungkut, Surabaya
+                </p>
+            </div>
+        </li>
+    );
+}
+
+function CompanyContact() {
+    return (
+        <li class="flex flex-col items-center justify-center">
+            <div class="flex h-10 w-10 items-center justify-center rounded bg-pink-900 dark:bg-pink-600 text-gray-50">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="h-6 w-6"
+                >
+                    <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"></path>
+                    <path d="M15 7a2 2 0 0 1 2 2"></path>
+                    <path d="M15 3a6 6 0 0 1 6 6"></path>
+                </svg>
+            </div>
+            <div>
+                <h3 class="py-3 mb-2 text-lg font-medium leading-6 text-gray-900 dark:text-white">
+                    Kontak
+                </h3>
+                <p class="text-gray-600 text-base dark:text-slate-400">
+                    Mobile: +62 811 333 6618
+                </p>
+                <p class="text-gray-600 text-base dark:text-slate-400">
+                    Office: 031-8786 2547
+                </p>
+            </div>
+        </li>
+    );
+}
+
+function CompanyWorkHour() {
+    return (
+        <li class="flex flex-col items-center justify-center">
+            <div class="flex h-10 w-10 items-center justify-center rounded bg-pink-900 dark:bg-pink-600 text-gray-50">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="h-6 w-6"
+                >
+                    <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path>
+                    <path d="M12 7v5l3 3"></path>
+                </svg>
+            </div>
+            <div>
+                <h3 class="py-3 mb-2 text-lg font-medium leading-6 text-gray-900 dark:text-white">
+                    Jam Kerja
+                </h3>
+                <p class="text-gray-600 text-base dark:text-slate-400">
+                    Senin - Jumat: 08:00 - 21:00
+                </p>
+                <p class="text-gray-600 text-base dark:text-slate-400">
+                    Sabtu: 08:00 - 15:00
+                </p>
+            </div>
+        </li>
+    );
+}
+
+function CompanyProfile() {
+    return (
+        <>
+            <MainTitleH2 title={"Tentang Kami"} />
+            <ul class="mb-6 md:mb-0 flex flex-col gap-8 justify-evenly">
+                <CompanyAddrs />
+                <CompanyContact />
+                <CompanyWorkHour />
+            </ul>
+        </>
+    );
+}
+
 function SectionContent({ title, useCard = false, useMap = false }) {
     let cardList;
     let mapComp;
@@ -331,9 +450,9 @@ export default function Homepage() {
             <Hero />
 
             <section id="product-us" class="pt-20 dark:bg-gray-900">
-                <SectionContent title={"Produk Kami"} useCard={true} />
-                <SectionContent title={"Toko Kami"} useMap={true} />
-                <SectionContent title={"Mengapa Kami"} />
+                <CompanyAdvantagesList />
+                <CompanyWorkImgs />
+                <CompanyProfile />
             </section>
 
             <Footer />
